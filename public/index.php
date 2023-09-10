026b7825345bc79bd50b05aef645fbc92199d521
yzPeedro <pedrocruzpessoa16@gmail.com>
2023-09-10 09:58:20 -0300

    var

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <?php
 
+ini_set('display_erros', true);
+chmod('/var', 0777);
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-chmod('/var', 0777)
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
