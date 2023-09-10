c4aca0a653800681c5a440e4c1d08ceea40f4826
Pedro Pessoa <54516747+yzPeedro@users.noreply.github.com>
2023-09-10 11:04:02 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
 
-umask(0000);
-
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+umask(0000);
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
