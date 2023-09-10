47e37474586f3b55be430a62ebc3a9695010a942
yzPeedro <pedrocruzpessoa16@gmail.com>
2023-09-10 09:56:24 -0300

    aaa

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+chmod('/var', 0777)
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
